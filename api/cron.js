813730678542cdb5d595a9d68e28c625d3a6799c
sh.chao <sh.chao@gmail.com>
2023-11-21 17:45:32 +0800

    Fix error

--- api/cron.js
+++ api/cron.js
@@ -1,8 +1,6 @@
-import { response } from "express";
-
 export default async function handler(request, response) {
-    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-      return res.status(401).end('Unauthorized');
+    if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+      return response.status(401).end('Unauthorized');
     }
     const result = await fetch(
       'http://worldtimeapi.org/api/timezone/America/Chicago',
