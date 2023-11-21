68910977a4e64368cb1fb0e872ba6086dfe38f9d
sh.chao <sh.chao@gmail.com>
2023-11-21 17:58:37 +0800

    Add Cron

--- api/cron.js
+++ api/cron.js
@@ -1,7 +1,7 @@
 export default async function handler(request, response) {
-    if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-      return response.status(401).end('Unauthorized');
-    }
+    // if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    //   return response.status(401).end('Unauthorized');
+    // }
     const result = await fetch(
       'http://worldtimeapi.org/api/timezone/America/Chicago',
     );
