0f69c9b55d5698176fae0674f1e9b4f1eac4745b
sh.chao <sh.chao@gmail.com>
2023-11-21 19:05:43 +0800

    Add cron.js in pages/api

---
+++ pages/api/cron.js
@@ -0,0 +1,19 @@
+export default async function handler(request, response) {
+    // if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    //   return response.status(401).end('Unauthorized');
+    // }
+    const result = await fetch(
+      'http://worldtimeapi.org/api/timezone/America/Chicago',
+    );
+    const data = await result.json();
+   
+    return response.json({ datetime: data.datetime });
+  }
+
+// export default function handler(req, res) {
+//     const url = 'https://gifbyme.glitch.com'
+//     fetch(url)
+//     .then(response => response.json)
+//     .then(data => {console.log(data)});
+//     res.status(200).end('Hello Cron!');
+// }
